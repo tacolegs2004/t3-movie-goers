ed52009aa6d9559727ef70094ce18e776a3d28c2
taco boi <tylerchall16@gmail.com>
2024-05-23 15:00:09 -0500

    fixed more stuff

--- src/app/_components/Search.tsx
+++ src/app/_components/Search.tsx
@@ -1,11 +1,11 @@
 "use client";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { useSearchParams } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export default function Search(props: { query: string }) {
   const searchParams = useSearchParams();
-  // const pathname = usePathname();
-  // const { replace } = useRouter();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
   function handleSearch(term: string) {
     const params = new URLSearchParams(searchParams);
@@ -14,7 +14,7 @@ export default function Search(props: { query: string }) {
     } else {
       params.delete("query");
     }
-    // replace(`${pathname}?${params.toString()}`);
+    replace(`${pathname}?${params.toString()}`);
   }
 
   return (
